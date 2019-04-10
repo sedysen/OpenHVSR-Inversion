9b6ec86a59cf23704b8f2774f0e71b5934542900
Samuel Bignardi <sedysen@gmail.com>
2019-04-10 11:41:03 -0400

    190410 history.m

--- src/history.m
+++
@@ -1,4 +0,0 @@
-working_folder = '/home/sedysen/Desktop/SIMONE/OPENHVSR/open hvsr - 4/';
-last_project_name = 'Openhvsr_project.m';
-last_elaboration_name = 'OpenHVSR_Inv__Elaboration_main.mat';
-last_log_number = 12;
